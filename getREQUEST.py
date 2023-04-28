d21030b50c9cb0c9918d86aefbffce8990611140
WooJae Kang <woojaeka@buffalo.edu>
2023-04-28 12:39:21 -0400

    Websocket frame parsing completed

--- getREQUEST.py
+++ getREQUEST.py
@@ -1,32 +1,27 @@
 import os
 import json
-import server
 from bson.json_util import dumps
 import sys
 from pymongo import MongoClient
 import hashlib
 import base64
+import random
+import html
+from server import MyTCPHandler
+from server import chat_messages
 
-# mongo_client = MongoClient("mongo")
-# db = mongo_client["cse312"]
-# logs_collection = db["logs"] #db.collection
-# chatlogs_collection = db["userComment"] # user : comment collection
-
-def getRESPONSE(request_path,databse, token,Accept_key, self):
+def getRESPONSE(request_path,databse,Accept_key, self):
     if request_path == "/hello":
         self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: 11\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\nHello World").encode())
-    
-    elif request_path == "/hi":
-        self.request.sendall(("HTTP/1.1 301 Moved Permanently\r\nContent-Length: 0\r\nLocation: /hello").encode())
 
     
     
     elif request_path == "/":
-        cxrf = token
+        
         html_file = open("index.html", "r").read()
         #sizeh = os.path.getsize("index.html")
         sizeinbyte = len(html_file)
-        html_file = html_file.replace("{{TOKEN}}", '<input value="' +cxrf + '" name="xsrf_token" hidden>')
+        # html_file = html_file.replace("{{TOKEN}}", '<input value="' +cxrf + '" name="xsrf_token" hidden>')
         html_file = html_file.replace("{{LOOP}}", databse)
         size = len(html_file)
         
@@ -41,62 +36,76 @@ def getRESPONSE(request_path,databse, token,Accept_key, self):
         temp = "HTTP/1.1 200 OK\r\nContent-Length: " + str(sizeinbyte) + "\r\nContent-Type: text/css; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\n" + css_file
         #print(temp)
         self.request.sendall(temp.encode())
-
-    elif request_path == "/functions.js":
-        js_file = open("functions.js").read()
-        #sizef = os.path.getsize("functions.js")
-        sizeinbyte = len(js_file)
-        
-        self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + str(sizeinbyte) + "\r\nContent-Type: text/javascript; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\n" + js_file).encode())
     
 
     elif request_path == "/new-javascript.js":
         js_file = open("new-javascript.js", "r").read()
         sizef = os.path.getsize('new-javascript.js')
         sizeinbyte = len(js_file)
-        print(sizef)
         self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + str(sizeinbyte + 3) + "\r\nContent-Type: text/javascript; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\n" + js_file).encode())
-
-
-    elif request_path == "/image/flamingo.jpg":
-        p = request_path[1:]
-        images = open(p, "rb").read()
-        #print(images)
-        self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\nX-Content-Type-Options: nosniff\r\n\r\n".encode() + images))
-
-    
-    
-    # elif "/image" in request_path:
-    #     image_path = "/image"
-    #     p = request_path[1:]
-    #     # p = p[6:]
-    #     # p = p.replace("/","")
-    #     # p = image_path
-    #     images = open(p, "rb").read()
-    #     self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\nX-Content-Type-Options: nosniff\r\n\r\n".encode() + images))
-
-    elif "/Uimages" in request_path:
-        p = request_path[1:] 
-        p = p.replace("/","")
-        images = open(p, "rb").read()
-        self.request.sendall((("HTTP/1.1 200 OK\r\nContent-Length: " + str(len(images)) + "\r\nContent-Type: image/jpeg\r\nX-Content-Type-Options: nosniff\r\n\r\n").encode() + images))
-
-        #print(p)
-        
-        #print(images)
     
 
 
-    # HW3 LO1 code starts here
+    # # HW3 LO1 code starts here
     elif "/websocket" in request_path:
         WebSocket_Accept = Accept_key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
         WebSocket_Accept = hashlib.sha1(WebSocket_Accept.encode())
         WebSocket_Accept = base64.b64encode(WebSocket_Accept.digest())
         WebSocket_Accept += "\r\n\r\n".encode()
-        print(WebSocket_Accept)
+        # print(WebSocket_Accept)
         self.request.sendall("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ".encode() + WebSocket_Accept)
+        username = "User" + str(random.randint(0,1000))
+        print(username)
+        
+        MyTCPHandler.connections.add(self)
+        while True:
+            data = self.request.recv(2)
+            opcode = data[0] & 0b00001111
+            payload_len = data[1] & 0b01111111
 
+            if opcode == 0x08:
+                break
+            
+            masking_key = self.request.recv(4)
+            if payload_len < 126:
+                payload_data = self.request.recv(payload_len)
+            else:
+                break
+
+            unMask_payload_data = b''
+            for i in range(len(payload_data)):
+                unMasked_byte = payload_data[i] ^ masking_key[i % 4]
+                unMask_payload_data += bytes([unMasked_byte])
+            
+            message_from_client = json.loads(unMask_payload_data.decode("utf-8"))
+            print(MyTCPHandler.connections)
+            if message_from_client["messageType"] == "chatMessage":
+                form_message = {
+                    "messageType": "chatMessage",
+                    "username": username,
+                    "comment": message_from_client["comment"].replace('&', "&amp;").replace('<',"&lt").replace('>',"&gt;")
+                }
+                chat_messages.insert_one({'username': form_message["username"], 'comment' : form_message["comment"]})
+                
+                length = len(json.dumps(form_message).encode())
+                if length <= 125:
+                    header = bytearray([0x81, length])
+                final_frame = bytes(header) + json.dumps(form_message).encode()
+                
+                for each_client in MyTCPHandler.connections:
+                    try:
+                        each_client.request.sendall(final_frame)
+                    except:
+                        MyTCPHandler.connections.remove(each_client)
+                
+                
     elif "/chat-history" in request_path:
+        messages = []
+        for user_message in chat_messages.find({},{"_id":0}):
+            messages.append(user_message)
+        messages = json.dumps(messages).encode("utf-8")
+        length_message = len(messages)
+        self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + str(length_message) + "\r\nContent-Type: application/json; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\n").encode() + messages)
         
     
 
--- server.py
+++ server.py
@@ -6,6 +6,9 @@ import secrets
 from pymongo import MongoClient
 import hashlib
 import base64
+import json
+import html
+import random
 # docker-compose up --build (start server)
 # ctrl c (stop server)
 # http://localhost:8080/
@@ -15,25 +18,26 @@ logs_collection = db["logs"] #db.collection
 chatlogs_collection = db["userComment"] # user : comment collection
 image_counter_collection = db["count"] # counter to update the image name find_one and update_one
 image_counter_collection.insert_one({"counter" : 0})
+chat_messages = db["chat_messages"] # stores user messages
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
+    connections = set()
     def handle(self):
         #dictionary to parse key : value pair
         
         
-        xsrf = secrets.token_hex(10)
 
         received_data = self.request.recv(2048) # data get from server
         #.split("\r\n",1)
         # bytes of a request
         
-        bytes = received_data
-        separator = bytes.find(b'\r\n\r\n')
-        bHeader = bytes[:separator]
-        bBody = bytes[separator + 4:]
+        bytess = received_data
+        separator = bytess.find(b'\r\n\r\n')
+        bHeader = bytess[:separator]
+        bBody = bytess[separator + 4:]
         data = bHeader.decode()
-        print(data)
+        # print(data)
 
         #parsing data
         
@@ -67,83 +71,11 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     if "Sec-WebSocket-Key:" in lines:
                         keys = lines.split(" ")[1]
             print(keys.encode()) 
-            # for line in request_body:
-            #     if "Sec-WebSocket-Key:" in line:
-            #         key = line.split(" ")[1]
+                        
+                
+            getREQUEST.getRESPONSE(line[1], replace_html, keys, self)
+
             
-            #print(WebSocket_Accept)
-
-            getREQUEST.getRESPONSE(line[1], replace_html, xsrf,keys, self)
-
-        #POST REQUEST   
-        elif request_type == "POST":
-
-            contentLength = ""
-            contentType = ""
-            boundary = ""
-            user = ""
-            comment = ""
-            counter = 0
-            image = b''
-
-            if line[1] == "/image-upload":
-                PostHeader = bHeader.split(b'\r\n\r\n',1)[0]
-                PostHeader = PostHeader.decode()
-                PostBody = bBody
-                
-                # Parsing header
-                for line in PostHeader.split("\r\n"):
-                    if "Content-Length: " in line:
-                        contentLength = line.split(" ")[1] # ex. 354
-                    if "Content-Type: " in line:
-                        contentType = line.split(" ")
-                        boundary = contentType[2] # boundary=-----WebKitFormBoundary
-                        boundary = boundary[9:] #----WebKitFormBoundary
-                
-                length_Body = len(bBody)
-                encodedContentLength = contentLength.encode()
-                #print(encodedContentLength)
-                encodedBoundary = boundary.encode()
-                #print(encodedBoundary)
-                
-                while length_Body < int(encodedContentLength):
-                    bBody += self.request.recv(2048) #request more bytes
-                    length_Body = len(bBody)
-                Decoded_body = bBody.split(b'\r\n--' + encodedBoundary)
-                user = Decoded_body[0].split(b'\r\n\r\n')[1].decode().replace('&', "&amp;").replace('<',"&lt").replace('>',"&gt;")
-                comment = Decoded_body[1].split(b'\r\n\r\n')[1].decode().replace('&', "&amp;").replace('<',"&lt").replace('>',"&gt;")
-                
-
-                Arr_of_bytes = bBody.split(b'\r\n--' + encodedBoundary)
-                #print("here")
-                imageLocation = Arr_of_bytes[2].split(b'\r\n\r\n',1)[1]
-                #print("1") 
-                if len(imageLocation) != 0:
-                    
-                    
-                    #print("ad")
-                    count = image_counter_collection.find_one({}, {"_id" : 0})
-                    counter = count["counter"]
-                    #print("2")
-
-
-                    chatlogs_collection.insert_one({"images" : "Uimages" + str(counter) + ".jpg"})
-                    
-                    #print("3")
-                    with open("Uimages" + str(counter) + ".jpg", "wb") as file:
-                        file.write(imageLocation)
-                        
-                        #print("4")
-                    image_counter_collection.update_one({"counter" : counter}, {"$set" : {"counter" : counter + 1}})
-                        
-                        
-                        
-                #print(user)
-                #print(comment)
-                #print("test")
-            chatlogs_collection.insert_one({user : comment})
-            
-            self.request.sendall(("HTTP/1.1 302 Found\r\nContent-Length: 0\r\nLocation:http://localhost:8080/").encode())
 
 
         print("\n")
